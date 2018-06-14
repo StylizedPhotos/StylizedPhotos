de3f88b3abe35e4c7416d6bac094b491215ec131
vadilat <vadilat@gmail.com>
2018-06-14 17:36:37 +0300

    sharpen 3 options + slider working + can restore base image

--- app/src/main/java/com/stylizedphotos/stylizedphotos/Sharpening.java
+++ app/src/main/java/com/stylizedphotos/stylizedphotos/Sharpening.java
@@ -19,14 +19,14 @@ public class Sharpening {
         filterScreenContext = filterScreen;
         rs = RenderScript.create(filterScreen);
         SeekBar s1 = new SeekBar(filterScreen);
-        s1.setMax(2);
+        s1.setMax(3);
         s1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
                 //  MyTaskParams params = new MyTaskParams(bitmap,seekBar.getProgress());
                 // new Background().execute(params);
 
-                filterScreen.RefreshImage(FilterFunction(bitmap));
+                filterScreen.RefreshImage(FilterFunction(bitmap,seekBar.getProgress()));
             }
 
             @Override
@@ -43,66 +43,48 @@ public class Sharpening {
         n1.setText("s1");
         names.add(n1);
         slider_array.add(s1);
-        SeekBar s2 = new SeekBar(filterScreen);
-        s2.setMax(4);
-        s2.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-                // TODO Auto-generated method stub
-
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-                // TODO Auto-generated method stub
-            }
-
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                // MyTaskParams params = new MyTaskParams(bitmap,seekBar.getProgress());
-                // new Background().execute(params);
-                //filterScreen.RefreshImage(FilterFunction(bitmap));
-            }
-        });
-        TextView n2 = new TextView(filterScreen);
-        n2.setText("s2");
-        names.add(n2);
-        slider_array.add(s2);
-        SeekBar s3 = new SeekBar(filterScreen);
-        s3.setMax(6);
-        s3.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-                // TODO Auto-generated method stub
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-                // TODO Auto-generated method stub
-            }
-
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                // MyTaskParams params = new MyTaskParams(bitmap,seekBar.getProgress());
-                // new Background().execute(params);
-            }
-        });
-        TextView n3 = new TextView(filterScreen);
-        n3.setText("s3");
-        names.add(n3);
-        slider_array.add(s3);
     }
 
-    private static Bitmap FilterFunction(Bitmap image)
+    private static Bitmap FilterFunction(Bitmap image,int strength)
     {
-        float arr[][]={ {0,-1,0},
+        float arr1[][]={ {0,-1,0},
                         {-1,5,-1},
                         {0,-1,0}};
-        Matrix ker = new Matrix (3,3,arr);
-      //  Bitmap temp_bitmap = HelpMethods.pad(image,1,1);
-      //  temp_bitmap = Matrix.convolution(ker,temp_bitmap,false);
-     //   temp_bitmap = Bitmap.createBitmap(temp_bitmap, 1, 1, image.getWidth(), image.getHeight());
-        return Matrix.convolution(ker, image,false);
+
+        float arr2[][]={{0,0,-1,0,0},
+                {0,0,-1,0,0},
+                {-1,-1,9,-1,-1},
+                {0,0,-1,0,0},
+                {0,0,-1,0,0}};
+
+        float arr3[][]={{0,0,0,-1,0,0,0},
+                {0,0,0,-1,0,0,0},
+                {0,0,0,-1,0,0,0},
+                {-1,-1,-1,13,-1,-1,-1},
+                {0,0,0,-1,0,0,0},
+                {0,0,0,-1,0,0,0},
+                {0,0,0,-1,0,0,0}};
+        Matrix ker = new Matrix(0, 0,null );
+        switch (strength){
+            case(0):{
+            }
+            case(1): {
+                ker = new Matrix(3, 3, arr1);
+                break;
+            }
+            case(2): {
+                ker = new Matrix(5, 5, arr2);
+                break;
+            }
+            case(3): {
+                ker = new Matrix(7, 7, arr3);
+                break;
+            }
+        }
+        if(strength!=0)
+            return Matrix.convolution(ker,image,false);
+        else
+            return image;
     }
 
     private void RefreshImage(Bitmap bitmap) {

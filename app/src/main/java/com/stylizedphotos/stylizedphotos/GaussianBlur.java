2d925f57e92bab2120ba60b6a0d2b5b4288b8856
itai marom <itaimarom@gmail.com>
2018-06-30 17:33:48 +0300

    fixed external filter. add more gaussian levels

--- app/src/main/java/com/stylizedphotos/stylizedphotos/GaussianBlur.java
+++ app/src/main/java/com/stylizedphotos/stylizedphotos/GaussianBlur.java
@@ -19,11 +19,11 @@ public class GaussianBlur {
         filterScreenContext = filterScreen;
         rs = RenderScript.create(filterScreen);
         SeekBar s1 = new SeekBar(filterScreen);
-        s1.setMax(2);
+        s1.setMax(3);
         s1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-                filterScreen.RefreshImage(FilterFunction(bitmap));
+                filterScreen.RefreshImage(FilterFunction(bitmap, seekBar.getProgress()));
             }
 
             @Override
@@ -40,13 +40,39 @@ public class GaussianBlur {
         slider_array.add(s1);
     }
 
-    private static Bitmap FilterFunction(Bitmap image)
+    private static Bitmap FilterFunction(Bitmap image, int strength)
     {
-        float arr1[][] = {{1},{76},{1992},{20199},{80576},{127641},{80576},{20199},{1992},{76},{1}};
-        float arr2[][] = {{1,76,1992,20199,80576,127641,80576,20199,1992,76,1}};
-        Matrix ker1 = new Matrix(11,1,arr1);
-        Matrix ker2 = new Matrix(1,11,arr2);
-        return Matrix.convolution(ker2, Matrix.convolution(ker1,image,true),true);
+        float arr1[][] = {{1}, {10}, {20}, {30}, {40}, {50}, {60}, {50}, {40}, {30}, {20}, {10}, {1}};
+        float arr2[][] = {{1, 10, 20, 30, 40, 50, 60, 50, 40, 30, 20, 10, 1}};
+        float arr3[][] = {{1}, {76}, {1992}, {20199}, {80576}, {127641}, {80576}, {20199}, {1992}, {76}, {1}};
+        float arr4[][] = {{1, 76, 1992, 20199, 80576, 127641, 80576, 20199, 1992, 76, 1}};
+        float arr5[][] = {{1}, {4}, {10}, {22}, {10}, {4}, {1}};
+        float arr6[][] = {{1, 4, 10, 22, 10, 4, 1}};
+        Matrix ker1 = new Matrix(13, 1, arr1);
+        Matrix ker2 = new Matrix(1, 13, arr2);
+        switch (strength) {
+            case (0): {
+            }
+            case (1): {
+                ker1 = new Matrix(7, 1, arr5);
+                ker2 = new Matrix(1, 7, arr6);
+                break;
+            }
+            case (2): {
+                ker1 = new Matrix(11, 1, arr3);
+                ker2 = new Matrix(1, 11, arr4);
+                break;
+            }
+            case (3): {
+                ker1 = new Matrix(13, 1, arr1);
+                ker2 = new Matrix(1, 13, arr2);
+                break;
+            }
+        }
+        if (strength != 0)
+            return Matrix.convolution(ker2, Matrix.convolution(ker1, image, true), true);
+        else
+            return image;
     }
 
     private void RefreshImage(Bitmap bitmap) {
